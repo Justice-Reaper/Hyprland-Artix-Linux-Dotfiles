0922d5d0df15ed0c45b07d1928596fcd3cbf880c
Sergio Aledo Bernal <justice.reaper.io@gmail.com>
2026-07-13 23:41:23 +0200

    Update hyprland.lua

--- config/hypr/hyprland.lua
+++ config/hypr/hyprland.lua
@@ -59,9 +59,7 @@ end)
 -- See https://wiki.hypr.land/Configuring/Advanced-and-Cool/Environment-variables/
 
 hl.env("XCURSOR_SIZE", "24")
-hl.env("HYPRCURSOR_SIZE", "24")
 hl.env("XCURSOR_THEME", "Windows-10-Alt-Light")
-hl.env("HYPRCURSOR_THEME", "Windows-10-Alt-Light")
 hl.env("QT_QPA_PLATFORMTHEME", "qt5ct")
 hl.env("QT_QPA_PLATFORM", "wayland;xcb")
 
